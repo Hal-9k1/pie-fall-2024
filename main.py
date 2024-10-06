4ea6bb150c01d6d62957a433485538c04b0b57ea
Eddy Meals <edimea211@gmail.com>
2024-10-06 00:59:09 -0700

    Write proof of concept for layer system
    Add mock_robot module from the spring comp.
    Print a restaurant menu to demonstrate the divide-and-conquer strategy
    with the layer system.

--- main.py
+++ main.py
@@ -1,26 +1,45 @@
-from layer.drive import TwoWheelDrive
-from layer import SimpleDriveTest
-from layer.controls import TankDriveControls
-from layer.controls import GamepadInputGenerator
+#from layer.drive import TwoWheelDrive
+#from layer import SimpleDriveTest
+#from layer.controls import TankDriveControls
+#from layer.controls import GamepadInputGenerator
+from layer.test import *
 from controller import RobotController
+from mock_robot import MockRobot
 
-robot = Robot
+try:
+    robot = Robot
+    is_dawn = True
+except NameError:
+    robot = MockRobot({
+        "koalabear": 0,
+        "servocontroller": 0,
+    })
+    is_dawn = False
 auto_layer_classes = [
-    TwoWheelDrive,
-    SimpleDriveTest,
+#    TwoWheelDrive,
+#    SimpleDriveTest,
+    RestaurantPrinter,
+    RestaurantSectionLayer,
+    RestaurantMenuSource,
 ]
 teleop_layer_classes = [
-    TwoWheelDrive,
-    TankDriveControls,
-    GamepadInputGenerator,
+#    TwoWheelDrive,
+#    TankDriveControls,
+#    GamepadInputGenerator,
 ]
 robot_controller = RobotController(robot)
 
+@_PREP_ENTRY_POINT
 def autonomous_setup():
     robot_controller.setup(auto_layer_classes)
+@_PREP_ENTRY_POINT
 def autonomous_main():
-    robot_controller.update()
+    if robot_controller.update() and not is_dawn:
+        exit(0)
+@_PREP_ENTRY_POINT
 def teleop_setup():
     robot_controller.setup(teleop_layer_classes)
+@_PREP_ENTRY_POINT
 def teleop_main():
-    robot_controller.update()
+    if robot_controller.update() and not is_dawn:
+        exit(0)
