d5eb32676c536ad19ea7836bbe1a66f69db2db07
Eddy Meals <edimea211@gmail.com>
2024-10-06 00:08:27 -0700

    Change controller update listener semantics

--- controller.py
+++ controller.py
@@ -21,14 +21,16 @@ class RobotController:
         self._layers = [Class(self._layer_setup_info) for Class in layer_classes]
 
     def update(self):
-        """Performs incremental work.
+        """Performs incremental work and returns whether layers have completed all tasks.
 
         Performs incremental work on the bottommost layer, invoking upper layers as necessary when
-        lower layers complete their current tasks.
+        lower layers complete their current tasks. Returns whether the topmost layer (and by
+        extension, every layer) are exhausted of tasks. When this happens, update listeners are
+        notified and then unregistered.
         """
         # Call all update listeners
         for listener in self._update_listeners:
-            listener()
+            listener(False)
 
         # Do work on layers
         i = 0 # Fallback in case of 0 layers which shouldn't happen anyways
@@ -39,8 +41,25 @@ class RobotController:
                 break
         if task is self:
             # No tasks left in any layer
-            return
+            for listener in self._update_listeners:
+                listener(True)
+            self._update_listeners = []
+            return True
         # Start at layer below first incomplete one and continue backwards to lowest layer
         for j in range(i - 1, -1, -1):
             self._layers[j].accept_task(task)
             task = self._layers[j].update()
+        return False
+
+    def add_update_listener(self, listener):
+        """Registers a function to be called on every update().
+
+        Registers a function to be called on every update of the controller before layer work is
+        performed. Listeners are executed in registration order and called with a single positional
+        argument of True. On the first update after the topmost layer runs out of tasks, the
+        listeners are called again with an argument of False, then unregistered.
+
+        Positional arguments:
+        listener -- the function to be registered as an update listener
+        """
+        self._update_listeners.append(listener)
